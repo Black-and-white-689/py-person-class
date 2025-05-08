95946e303319a47645a704b1e2d9c17ca62e1d35
Ihor Pershin <issue.07.08.09@gmail.com>
2025-05-08 18:35:10 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
+
     person_list = []
     for i in people:
         person = Person(i["name"], i["age"])
